0754a924db08aff63b7f020914e72d7f665e7976
Otavin23 <alvesotavio132@gmail.com>
2022-07-12 21:16:56 -0300

    Home

---
+++ my-launch-coutdown/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Home from "./page/home";
+
+
+export const App = () => {
+    return (
+        <Home />
+    )
+}
+export default App
---
+++ my-launch-coutdown/src/styles/globalstyle.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+
+
+export const CreateGlobalStyle = createGlobalStyle`
+
+    *{
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+    body{
+        
+    }
+`
